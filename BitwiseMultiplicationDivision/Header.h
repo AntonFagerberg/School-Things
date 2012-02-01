06585c72db620d463ca107fd2d5a192d38c89e44
Anton Fagerberg <anton@antonfagerberg.com>
2012-02-01 18:16:29 +0100

    Bitwise multiplication and division.
    First shitty upload.
    Does bitwise multiplication and division in C

---
+++ BitwiseMultiplicationDivision/Header.h
@@ -0,0 +1,11 @@
+/* This is my first C-app so it might contain weird stuff.
+ * By Anton Fagerberg [ anton@antonfagerberg.com ]
+ * No license since it is not useful.
+ */
+#ifndef CTest_Header_h
+#define CTest_Header_h
+
+int multiplication(int factor1, int factor2);
+int division(int dividend, int divisor);
+
+#endif
