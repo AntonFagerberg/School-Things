3b7e0c3249cffa17c606342cf49fedbc2ad3ec9e
AntonFagerberg <anton@antonfagerberg.com>
2012-02-08 23:02:36 +0100

    Trying to fix indentation.

--- Assembler-EIT070/Laboration 2 Home 1.asm
+++ Assembler-EIT070/Laboration 2 Home 1.asm
@@ -5,7 +5,7 @@
 .globl start
 
 .ent start
-start:	jal		wait 			# Wait for button click
+start:		jal		wait 			# Wait for button click
 		nop
 		lui		s0, 0xbf90		# Load switch port address
 		lb		s1, 0x0(s0)		# Read first number from switches
@@ -18,17 +18,17 @@ start:	jal		wait 			# Wait for button click
 		sb		s3, 0x0(s0)		# Write the result to LEDs
 		b		start			# Repeat all over again
 
-wait:	lui 	a0, 0xbfa0		# Load K2 button address
-		addi	t2, r0, 0x1		# Create mask 00...001
+wait:		lui 		a0, 0xbfa0		# Load K2 button address
+		addi		t2, r0, 0x1		# Create mask 00...001
 		lb		t0, 0x0(a0)		# Load byte from Interrupt address
 		nop					
 		and		t1, t2, t0		# Mask out LSB
-		beq		t1, r0, wait	# Loop if LSB == 0
+		beq		t1, r0, wait		# Loop if LSB == 0
 		nop
-rel:	lb		t0, 0x0(a0)		# Load byte from Interrupt address
+rel:		lb		t0, 0x0(a0)		# Load byte from Interrupt address
 		nop						
 		and		t1, t2, t0		# Mask out LSB
 		bne		t1, r0, rel		# Loop if LSB == 1
 		nop
-		j		r31				# Jump to return address
+		j		r31			# Jump to return address
 .end start
